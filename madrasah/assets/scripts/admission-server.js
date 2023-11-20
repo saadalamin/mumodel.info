ccfb3005b72a97c49d0bc03e2aab286092f0bb76
mosefatullah <mosefatullah@gmail.com>
2023-11-20 16:04:40 +0600

    .

--- madrasah/assets/scripts/admission-server.js
+++ madrasah/assets/scripts/admission-server.js
@@ -8,15 +8,17 @@ import {
  getDatabase,
  ref as dbRef,
  set,
+ push,
 } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
 const db = getDatabase(app);
+const alertBox = document.getElementById("alertBox");
 
 document.getElementById("submitFormInput").onkeydown = function (e) {
  if (e.key == "Enter") {
-  const alertBox = document.getElementById("alertBox");
   const form = document.querySelector("#admissionForm .form");
   let data = {};
+  alertBox.innerHTML = "<span class='text-muted'>Please wait...</span>";
   for (let i = 0; i < form.length; i++) {
    if (form[i].tagName == "INPUT") {
     let n = form[i].placeholder
@@ -43,20 +45,25 @@ document.getElementById("submitFormInput").onkeydown = function (e) {
    "admissionAcademicYear"
   ).value;
   data["date"] = document.getElementById("date").value;
-  if (!validateForm(data)) return;
+  //if (!validateForm(data)) return;
   alertBox.innerHTML = "";
-  let mobile = "880"+document.querySelector('#submit input[type="number"]').value;
+  let mobile =
+   "880" + document.querySelector('#submit input[type="number"]').value;
   if (mobile.length < 10) {
    alertBox.innerHTML = "<span style='color: red'>Invalid mobile number</span>";
    return;
   }
-
-  let ref = dbRef(db, "admissions/applications/" + mobile);
-  set(ref, data)
+  data["apply_mobile"] = mobile;
+  let ref = push(dbRef(db, "admissions/apply/"));
+  let id = ref.key.replace(/-/g, "").replace(/_/g, "");
+  data["id"] = id;
+  set(dbRef(db, "admissions/apply/" + id), data)
    .then(() => {
-    alert("Admission form submitted successfully");
-    modalAdmission.hide();
-    modalAdmissionSave.hide();
+    document.getElementById("submitFormInput").disabled = true;
+    alertBox.innerHTML =
+     "<span style='color:green'>Admission form submitted successfully!<br/>Your application number is <span style='color: black'>" +
+     id +
+     "</span></span>";
     clearForm();
    })
    .catch((e) => {
