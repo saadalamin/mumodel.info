1c93d90bd184a4c8c1e21039fb218b4308ecf2a6
saadalamin <saadalamin2004@gmail.com>
2023-12-21 15:11:05 +0600

    .

--- school/assets/scripts/notices.js
+++ school/assets/scripts/notices.js
@@ -22,10 +22,8 @@ window.onload = async () => {
         snippet += `<li class="list-group-item d-flex notice"><div class="me-3 bg-white text-center" style="font-size: 0.8rem; width: min-content; text-transform: uppercase;">${
           date &&
           `<span class="d-block bg-danger text-white p-2 py-0" style="border-radius: 0.25rem 0.25rem 0 0;">${month}</span><span class="d-block p-2 py-0">${day}</span><span class="d-block bg-dark-subtle p-2 py-0" style="border-radius: 0 0 0.25rem 0.25rem; font-size: 0.67rem;">${dayName}</span>`
-        }</div><div><p class="p-0 lh-1">${
-          n.noticeLink ? `<a href="${n.noticeLink}">${n.title}</a>` : n.title
-        }</p><p class="p-0 text-muted" style="font-size: 0.75rem;overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2;  line-clamp: 2; -webkit-box-orient: vertical;">${
-          n.body?.replaceAll("\\n", "") || ""
+        }</div><div><p class="p-0 lh-1"><a href="search/?notice=${n.id}">${n.title}</a></p><p class="p-0 text-muted" style="font-size: 0.75rem;overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2;  line-clamp: 2; -webkit-box-orient: vertical;">${
+          n.body?.replaceAll("\\n", '') || ""
         }</p><p class="p-0 text-muted" style="font-size: 0.75rem;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ccc" class="bi bi-clock-fill" viewBox="0 0 18 18">
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
   </svg> Published: ${n.publishedDate}</p></div></li>`;
